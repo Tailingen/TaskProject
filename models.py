f245159d6314d2807261e9bd1a89a7522cca006d
Vladislav <Tailingen@mail.ru>
2025-07-07 16:06:19 +0500

    modify all functions for sqlalchemy

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import asyncio
+
+from sqlalchemy import select, insert, update
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.testing.schema import mapped_column
@@ -21,5 +23,19 @@ async def create_db():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+    async with engine.connect() as conn:
+        query = select(TaskModel).filter(TaskModel.id == 1)
+        res = await conn.execute(query)
+        print(res.first())
+        # print(query.compile(compile_kwargs={'literal_binds': True}))
+    # async with engine.connect() as conn:
+    #     query = insert(TaskModel).values(id=4, text='Купить шляпу', status=False)
+    #     await conn.execute(query)
+    #     await conn.commit()
+    # async with engine.connect() as conn:
+    #     query = update(TaskModel).filter(TaskModel.id == 1).values(text='Постричь кактус!')
+    #     await conn.execute(query)
+    #     await conn.commit()
 
-asyncio.run(create_db())
+
+# asyncio.run(create_db())
