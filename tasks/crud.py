eb5848d27ebbd82b4ff0d95d96383c3aa7c38656
Vladislav <Tailingen@mail.ru>
2025-07-13 14:55:26 +0500

    refactoring code

---
+++ tasks/crud.py
@@ -0,0 +1,37 @@
+from sqlalchemy import select, insert, update, delete
+
+from models import engine, TaskModel
+
+
+async def read_task(task_id: int):
+    async with engine.connect() as conn:
+        query = select(TaskModel).filter(TaskModel.id == task_id)
+        res = await conn.execute(query)
+    return list(res.first())
+
+async def read_tasks():
+    async with engine.connect() as conn:
+        query = select(TaskModel)
+        res = await conn.execute(query)
+    return [list(i) for i in res.all()]
+
+async def create_task(schema):
+    async with engine.connect() as conn:
+        query = insert(TaskModel).values(text=schema.text, status=schema.status)
+        await conn.execute(query)
+        await conn.commit()
+    return schema
+
+async def update_task(task_id: int, schema):
+    async with engine.connect() as conn:
+        query = update(TaskModel).filter(TaskModel.id == task_id).values(text=schema.text, status=schema.status)
+        await conn.execute(query)
+        await conn.commit()
+    return schema
+
+async def delete_task(task_id: int):
+    async with engine.connect() as conn:
+        query = delete(TaskModel).filter(TaskModel.id == task_id)
+        await conn.execute(query)
+        await conn.commit()
+    return {'status': 'Запись удалена'}
--- tasks/views.py
+++ tasks/views.py
@@ -2,44 +2,32 @@ from fastapi import APIRouter
 from sqlalchemy import insert, update, delete, select
 
 from models import engine, TaskModel
+from tasks.crud import read_task, read_tasks, create_task, update_task, delete_task
 from tasks.schemas import TaskAddSchema
 
 router = APIRouter(prefix='/tasks', tags=['tasks', ])
 
 @router.get('/{task_id}/')
 async def task(task_id: int):
-    async with engine.connect() as conn:
-        query = select(TaskModel).filter(TaskModel.id == task_id)
-        res = await conn.execute(query)
-    return list(res.first())
+    res = await read_task(task_id)
+    return res
 
 @router.get('/')
 async def tasks():
-    async with engine.connect() as conn:
-        query = select(TaskModel)
-        res = await conn.execute(query)
-    return [list(i) for i in res.all()]
+    res = await read_tasks()
+    return res
 
 @router.post('/create/')
 async def task_create(schema: TaskAddSchema):
-    async with engine.connect() as conn:
-        query = insert(TaskModel).values(text=schema.text, status=schema.status)
-        await conn.execute(query)
-        await conn.commit()
-    return schema
+    res = await create_task(schema)
+    return res
 
 @router.put('/{task_id}/update/')
 async def task_update(task_id: int, schema: TaskAddSchema):
-    async with engine.connect() as conn:
-        query = update(TaskModel).filter(TaskModel.id == task_id).values(text=schema.text, status=schema.status)
-        await conn.execute(query)
-        await conn.commit()
-    return schema
+    res = await update_task(task_id, schema)
+    return res
 
 @router.delete('/{task_id}/delete/')
 async def task_delete(task_id: int):
-    async with engine.connect() as conn:
-        query = delete(TaskModel).filter(TaskModel.id == task_id)
-        await conn.execute(query)
-        await conn.commit()
-    return {'status': 'Запись удалена'}
+    res = await delete_task(task_id)
+    return res
